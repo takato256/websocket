65b40e8ef194b1c63c54dc70208ecc1eff96b962
takato256 <c1139952@planet.kanazawa-it.ac.jp>
2024-01-07 07:24:51 +0900

    [追加] 部屋を作る機能を実装

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,17 +1,26 @@
 <template>
   <div id="app">
-    <div>名前: <input v-model="userName" type="text" /></div>
-
-    <input type="radio" v-model="joinType" value="1" />新しく部屋を作る
-    <input type="radio" v-model="joinType" value="2" />友達の部屋に入る
-
-    <div v-if="joinType == 1">
-      <input type="button" value="部屋を作る" @click="createRoom" />
+    <!--入室済みの場合(部屋の情報を表示)-->
+    <div v-if="isJoined">
+      <div>{{ userName }}さん</div>
+      部屋番号: {{ roomId }}
     </div>
 
-    <div v-if="joinType == 2">
-      部屋番号: <input v-model="roomId" type="text" />
-      <input type="button" value="入室" @click="enterRoom" />
+    <!--未入室の場合(部屋を作る or 部屋に入る)-->
+    <div v-else>
+      <div>名前: <input v-model="userName" type="text" /></div>
+
+      <input type="radio" v-model="joinType" value="1" />新しく部屋を作る
+      <input type="radio" v-model="joinType" value="2" />友達の部屋に入る
+
+      <div v-if="joinType == 1">
+        <input type="button" value="部屋を作る" @click="createRoom" />
+      </div>
+
+      <div v-if="joinType == 2">
+        部屋番号: <input v-model="roomId" type="text" />
+        <input type="button" value="入室" @click="enterRoom" />
+      </div>
     </div>
   </div>
 </template>
@@ -24,12 +33,29 @@ export default {
     useName: "",
     joinType: 1,
     roomId: "",
+    isJoined: false,
     socket: io("http://localhost:3031")
   }),
 
   created() {
     this.socket.on("connect", () => {
       console.log("connected");
+    });
+  },
+
+  methods: {
+    createRoom() {
+      this.socket.emit("create", this.userName);
+    },
+    enterRoom() {
+      this.socket.emit("enter", this.userName, this.roomId);
+    }
+  },
+
+  mounted() {
+    this.socket.on("updateRoom", (room) => {
+      this.isJoined = true;
+      this.roomId = room.id;
     });
   }
 };
--- server/index.js
+++ server/index.js
@@ -6,3 +6,42 @@ const io = require("socket.io")(http, {
 });
 
 http.listen(3031);
+
+const rooms = [];
+const users = [];
+
+io.on("connection", (socket) => {
+    // 部屋を新しく建てる
+    socket.on("create", (userName) => {
+        // ランダムな部屋IDを生成
+        const roomId = generateRoomId();
+
+        // ユーザー・部屋の情報をそれぞれデータとして格納する
+        const user = { id: socket.id, name: userName, roomId};
+        const room = {
+            id: roomId,
+            users: [user],
+            turnUserIdex: 0,
+            posts: []
+        };
+        rooms.push(room);
+        users.push(user);
+
+        // 部屋に入る
+        socket.join(roomId);
+
+        // 部屋情報をクライアントに送る
+        // emit()はSocket.IOのイベント送信処理
+        io.to(socket.id).emit("updateRoom", room);
+    });
+});
+
+// ランダムなroomId(1000-9999)を生成する
+function generateRoomId() {
+    const id = Math.floor(Math.random() * 8999 + 1000);
+    // ランダムに生成したidが既に存在する場合は作り直す
+    if (rooms.some((r) => r.id == id)) {
+        return generateRoomId();
+    }
+    return id;
+}
